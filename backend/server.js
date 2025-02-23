0f73ec647785275bd67a08287edf8b78191e64ec
Bhaskar Kulshrestha <bhaskarkulshrestha03@gmail.com>
2025-02-24 01:44:50 +0530

    docker - frontend wroking not backend

--- backend/server.js
+++ backend/server.js
@@ -52,10 +52,10 @@ app.post("/api/run-code", async (req, res) => {
 
 
 // 🔹 Serve Frontend (Production Build)
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
 });
 
 // 🔹 Start Server
